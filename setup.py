57b962337a77ae80326e8bcb84d7b373a5310981
André Claudino <andre.claudino@b2wdigital.com>
2019-10-09 20:44:37 -0300

    Start Python project

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='dataset_loader',
+    version='',
+    packages=['data'],
+    url='',
+    license='',
+    author="André Claudino",
+    author_email='',
+    description=''
+)
