82c045b4ec80e3b7570fe865acbcae22bfdf8025
André Claudino <claudino@d2x.com.br>
2019-10-10 15:41:17 -0300

    Add publishing data

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 from distutils.core import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("LICENSE", "r") as fh:
+    license = fh.read()
+
+
 setup(
     name='dataset-loader',
     version='0.1',
     packages=['data_loader'],
-    url='',
-    license='',
+    license=license,
+    long_description=long_description,
     author="André Claudino",
-    author_email='',
-    description='',
+    long_description_content_type="text/markdown",
+    url="https://github.com/andreclaudino/dataset_loader",
+    description="A simple package to load folder partitioned data",
     install_requires=['pandas']
 )
